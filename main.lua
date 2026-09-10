f63c504edf0fd035bd4a0f038fff54c59ba17783
Bulka <kwadu@matrix.org>
2026-09-10 20:27:37 +0200

    kolizje

--- main.lua
+++ main.lua
@@ -1,30 +1,39 @@
 function love.load()
 	love.window.setMode(1024, 768, {vsync= 1, resizable = true, minwidth = 640, minheight = 480})
 	love.window.setTitle("Speed maze")
-	level = 1
 	boxes = {}
 	boxes[1] = {0,0}
-	boxes[2] = {0,1}
+	boxes[2] = {0,0}
+	level = #boxes - 1
 
 end
 
 function love.update(dt)
-end
-
-function love.draw()
 	wid, hei = love.window.getMode()
 	if wid > hei then
 		gbox = hei*0.95
 	else
 		gbox = wid+0.95
 	end
+	gap = gbox/((#boxes)*4 + level+2)
 
+	mx, my = love.mouse.getPosition()
+	for i,v in ipairs(boxes) do
+		for j,w in ipairs(v) do
+			if collision(((wid-gbox)/2)+gap+(gap*5*(i-1)),((hei-gbox)/2)+gap+(gap*5*(j-1)),gap*4,gap*4,mx,my,0,0) then
+				boxes[i][j] = 1
+			else
+				boxes[i][j] = 0
+			end
+		end
+	end
+end
+
+function love.draw()
 	love.graphics.setColor(218/255,221/255,210/255)
 	love.graphics.rectangle("fill",0,0,wid,hei)
 	love.graphics.setColor(230/225,238/225,231/225)
 	love.graphics.rectangle("fill",(wid-gbox)/2,(hei-gbox)/2,gbox,gbox,5,5)
-
-	gap = gbox/((level+1)*4 + level+2)
 
 	for i,v in ipairs(boxes) do
 		for j,w in ipairs(v) do
@@ -40,4 +49,11 @@ function love.draw()
 			love.graphics.rectangle("fill",((wid-gbox)/2)+gap+(gap*5*(i-1)),((hei-gbox)/2)+gap+(gap*5*(j-1)),gap*4,gap*4,5,5)
 		end
 	end
+end
+
+function collision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
 end
